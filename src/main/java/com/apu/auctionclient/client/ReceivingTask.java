95195a496c00874b97c1e026610f0a74968630c7
apu <pasha_anik@ukr.net>
2017-12-07 15:41:30 +0200

    Refactoring

--- src/main/java/com/apu/auctionclient/client/ReceivingTask.java
+++ src/main/java/com/apu/auctionclient/client/ReceivingTask.java
@@ -8,13 +8,8 @@ package com.apu.auctionclient.client;
 import com.apu.auctionapi.AuctionQuery;
 import com.apu.auctionclient.controller.NetworkController;
 import com.apu.auctionclient.entity.Message;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.Socket;
 import java.util.concurrent.BlockingQueue;
 import java.util.logging.Level;
@@ -41,43 +36,38 @@ public class ReceivingTask implements Runnable {
     @Override
     public void run() {
         InputStream is = null;
-//        OutputStream os = null;
         NetworkController controller = NetworkController.getInstance();
         try {
             try {
                 is = socket.getInputStream();
-//                os = socket.getOutputStream();
-//                BufferedReader in = new BufferedReader(new InputStreamReader(is));
-//                BufferedWriter out = new BufferedWriter(new OutputStreamWriter(os));
-                String line = null;
+                String line;
                 String str;
                 int amount;
                 StringBuilder sb = new StringBuilder();;
                 byte[] bytes = new byte[1024];
                 while(!socket.isClosed()) {                    
-                        if(Thread.currentThread().isInterrupted()) {
-                            System.out.println("Receiving thread. Interrupted.");
-                            break;
+                    if(Thread.currentThread().isInterrupted()) {
+                        System.out.println("Receiving thread. Interrupted.");
+                        break;
+                    }
+                    if(is.available() == 0) continue;
+                    amount = is.read(bytes, 0, 1024);
+                    str = new String(bytes, 0, amount);
+                    sb.append(str);
+                    if(sb.toString().contains("\r\n")) {
+                        line = sb.toString();
+                        sb.delete(0, sb.capacity());
+                        if(line != null) {
+                            System.out.println(line);
+                            controller.handle(line);
                         }
-                        if(is.available() == 0) continue;
-                        amount = is.read(bytes, 0, 1024);
-                        str = new String(bytes, 0, amount);
-                        sb.append(str);
-                        if(sb.toString().contains("\r\n")) {
-                            line = sb.toString();
-                            sb.delete(0, sb.capacity());
-                            if(line != null) {
-                                System.out.println(line);
-                                controller.handle(line);
-                            }
-                        }                   
+                    }                   
                 }
             } catch (Exception ex) {
                 Logger.getLogger(ReceivingTask.class.getName()).log(Level.SEVERE, null, ex);
                 System.out.println("Receiving thread. Message - Error");
                 messagesQueue.add(new Message("Error"));                
             } finally { 
-//                if(os != null)  os.close();
                 if(is != null) { 
                     is.close();
                     System.out.println("Receiving thread. Input socket closed");           
